f70947653fcfad9ec0c857e046ade7674266d028
cr4zys4mw1se <43831295+cr4zys4mw1se@users.noreply.github.com>
2019-06-18 01:15:16 -0700

    Update update.sh
    Main changes made:
    -Execute from any directory
    -General message/status output
    
    Tested with Ubuntu 16.04, 18.04

--- update.sh
+++ update.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
+#
+# Created by: cr4zys4mw1se
+#
+# Donation Addresses:
+# x42: XKCm56Q4GmxRYbk3aS8seAQaHdtUDpgEfx
+# BTC: 145QLo9jopWK3Z7h43fEgTy7AbUA239Ghz
+# LTC: LVkLucNkv43Zwpqx1Vb1mymnQR2YLPXzZR
+#
 
-if [ ! -d "X42-FullNode" ]; then
-    git clone https://github.com/x42protocol/X42-FullNode.git
+printf "\nChecking if the X42-FullNode folder is available...\n\n"
+if [ ! -d "/home/$USER/X42-FullNode" ]; then
+    printf "Folder not found, downloading from GitHub...\n\n"
+    git clone https://github.com/x42protocol/X42-FullNode.git ~/X42-FullNode
+    printf "\nStopping x42node.service\n\n"
     sudo systemctl stop x42node.service
-    cd X42-FullNode/src
+    printf "\nBuilding FullNode\n\n"
+    cd ~/X42-FullNode/src
     dotnet restore
     dotnet build --configuration Release
-    if [ ! -d "~/x42node" ]; then
+    if [ ! -d "/home/$USER/x42node" ]; then
         mkdir ~/x42node
+        mv ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
+    else
+        mv -f ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
     fi
-    mv -f ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
+    printf "\nStarting x42node.service\n\n"
     sudo systemctl start x42node.service
-    cd
-elif [ -d "X42-FullNode" ]; then
+    sleep 1.25
+    if [ "$(systemctl is-active x42node.service)" == "active" ]; then
+        printf "\nFinished.\n\nYou may now check your node using: curl http://127.0.0.1:42220/api/Dashboard/Stats\n\n"
+    else
+        printf "\nThere may be an error with the Node.\n\nCheck the Node status with: sudo systemctl status x42node.service\n\n"
+    fi
+else
+    printf "X42-FullNode folder found, stopping x42node.service\n\n"
     sudo systemctl stop x42node.service
-    cd X42-FullNode
+    printf "\nUpdating from GitHub...\n\n"
+    cd ~/X42-FullNode
     git pull origin master
+    printf "\nBuilding FullNode...\n\n"
     cd src
     dotnet restore
     dotnet build --configuration Release
-    if [ ! -d "~/x42node" ]; then
+    if [ ! -d "/home/$USER/x42node" ]; then
         mkdir ~/x42node
+        mv ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
+    else
+        mv -f ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
     fi
-    mv -f ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
+    printf "\nStarting x42node.service\n\n"
     sudo systemctl start x42node.service
-    cd
+    sleep 1.25
+    if [ "$(systemctl is-active x42node.service)" == "active" ]; then
+        printf "\nFinished.\n\nYou may now check your node using: curl http://127.0.0.1:42220/api/Dashboard/Stats\n\n"
+    else
+        printf "\nThere may be an error with the Node.\n\nCheck the Node status with: sudo systemctl status x42node.service\n\n"
+    fi
 fi
