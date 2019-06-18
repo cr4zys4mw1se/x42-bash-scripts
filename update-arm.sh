a0d69cb44c88e66facf0d1755a83e5f50a9ee935
cr4zys4mw1se <43831295+cr4zys4mw1se@users.noreply.github.com>
2019-06-18 14:48:25 -0700

    Update update-arm.sh
    Main changes made:
    -Asks for ARM device IP and Username (instead of manually editing script)
    -General message/status output
    -Execute from any directory
    
    Tested with old laptop (lubuntu 18.04) and rockpro64 (ubuntu 18.04)

--- update-arm.sh
+++ update-arm.sh
@@ -1,30 +1,84 @@
 #!/bin/bash
+#
+# Created by: cr4zys4mw1se
+#
+# Donation Addresses:
+# x42: XKCm56Q4GmxRYbk3aS8seAQaHdtUDpgEfx
+# BTC: 145QLo9jopWK3Z7h43fEgTy7AbUA239Ghz
+# LTC: LVkLucNkv43Zwpqx1Vb1mymnQR2YLPXzZR
+#
 
-if [ ! -d "X42-FullNode" ]; then
-    git clone https://github.com/x42protocol/X42-FullNode.git
-    cd X42-FullNode/src
+printf "\nChecking if the X42-FullNode folder is available...\n\n"
+notAvail=0
+if [ ! -d "/home/$USER/X42-FullNode" ]; then
+    ((notAvail+=20))
+    printf "Folder not found, downloading from GitHub...\n\n"
+    git clone https://github.com/x42protocol/X42-FullNode.git ~/X42-FullNode
+    printf "\nBuilding FullNode...\n\n"
+    cd ~/X42-FullNode/src
     dotnet restore
     dotnet publish --configuration Release
-    if [ ! -d "~/x42node" ]; then
-        mkdir ~/x42node
+    if [ ! -d "/home/$USER/x42node" ]; then
+        ((notAvail+=22))
+        mv x42.x42D/bin/Release/netcoreapp2.1/publish ~/x42node
+    else
+        mv -f x42.x42D/bin/Release/netcoreapp2.1/publish ~/x42node
     fi
-    mv -f ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/publish ~/x42node/
-    cd
-    ssh USERNAME@xxx.xxx.xxx.xxx 'sudo systemctl stop x42node.service; rm -rf x42node'
-    rsync -ahP ~/x42node USERNAME@xxx.xxx.xxx.xxx:/home/USERNAME
-    ssh USERNAME@xxx.xxx.xxx.xxx 'sudo systemctl start x42node.service'
-elif [ -d "X42-FullNode" ]; then
-    cd X42-FullNode
+    printf "\nPlease provide the following information for your ARM device.\n\n"
+    read -p "Username: " username
+    read -p "IP Address: " armIP
+    printf "\nStopping service and removing x42node folder from ARM device.\n\n"
+    ssh $username@$armIP 'sudo systemctl stop x42node.service; rm -rf ~/x42node'
+    printf "\nCopying updated x42node folder to ARM device.\n\n"
+    rsync -ahP ~/x42node $username@$armIP:/home/$username
+    printf "\nStarting x42node.service\n\n"
+    ssh $username@$armIP 'sudo systemctl start x42node.service'
+    printf "\nVerifying x42node.service is active...\n\n"
+    if [ "$(ssh $username@$armIP 'systemctl is-active x42node.service')" == "active" ]; then
+        printf "\nx42node.service started successfully.\n\n"
+    else
+        printf "\nx42node.service may have not started successfully.\n\nTry connecting to the ARM device and check the status, use: sudo systemctl status x42node.service\n\n"
+    fi
+    if [ "$notAvail" == "20" ]; then
+        rm -rf ~/X42-FullNode
+    elif [ "$notAvail" == "22" ]; then
+        rm -rf ~/x42node
+    elif [ "$notAvail" == "42" ]; then
+        rm -rf ~/X42-FullNode
+        rm -rf ~/x42node
+    fi
+    printf "\nExiting.\n\n"
+else
+    printf "X42-FullNode folder found. Updating now...\n\n"
+    cd ~/X42-FullNode
     git pull origin master
+    printf "\nBuilding FullNode...\n\n"
     cd src
     dotnet restore
     dotnet publish --configuration Release
-    if [ ! -d "~/x42node" ]; then
-        mkdir ~/x42node
+    if [ ! -d "/home/$USER/x42node" ]; then
+        ((notAvail+=42))
+        mv x42.x42D/bin/Release/netcoreapp2.1/publish ~/x42node
+    else
+        mv -f x42.x42D/bin/Release/netcoreapp2.1/publish ~/x42node
     fi
-    mv -f ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/publish ~/x42node/
-    cd
-    ssh USERNAME@xxx.xxx.xxx.xxx 'sudo systemctl stop x42node.service; rm -rf x42node'
-    rsync -ahP ~/x42node USERNAME@xxx.xxx.xxx.xxx:/home/USERNAME
-    ssh USERNAME@xxx.xxx.xxx.xxx 'sudo systemctl start x42node.service'
+    printf "\nPlease provide the following information for your ARM device.\n\n"
+    read -p "Username: " username
+    read -p "IP Address: " armIP
+    printf"\nStopping service and removing x42node folder from ARM device.\n\n"
+    ssh $username@$armIP 'sudo systemctl stop x42node.service; rm -rf ~/x42node'
+    printf "\nCopying updated x42node folder to ARM device.\n\n"
+    rsync -ahP ~/x42node $username@$armIP:/home/$username
+    printf "\nStarting x42node.service\n\n"
+    ssh $username@$armIP 'sudo systemctl start x42node.service'
+    printf "\nVerifying x42node.service is active...\n\n"
+    if [ "$(ssh $username@$armIP 'systemctl is-active x42node.service')" == "active" ]; then
+        printf "\nx42node.service started successfully.\n\n"
+    else
+        printf "\nx42node.service may have not started successfully.\n\nTry connecting to the ARM device and check the status, use: sudo systemctl status x42node.service\n\n"
+    fi
+    if [ "$notAvail" == "42" ]; then
+        rm -rf ~/x42node
+    fi
+    printf "\nExiting.\n\n"
 fi
