e9e70ecf0806a8bb9c193c22e012abf3f7f264b1
cr4zys4mw1se <43831295+cr4zys4mw1se@users.noreply.github.com>
2019-06-18 01:15:23 -0700

    Update install.sh
    Main changes made:
    -Execute from any directory
    -Verifies Ubuntu release
    -Installs .NET SDK based on Ubuntu release
    -Adjusted startup service creation
    
    Tested with Ubuntu 16.04, 18.04, 18.10, 19.04 (VM new setup/install).

--- install.sh
+++ install.sh
@@ -1,27 +1,108 @@
 #!/bin/bash
+#
+# Created by: cr4zys4mw1se
+#
+# Donation Addresses:
+# x42: XKCm56Q4GmxRYbk3aS8seAQaHdtUDpgEfx
+# BTC: 145QLo9jopWK3Z7h43fEgTy7AbUA239Ghz
+# LTC: LVkLucNkv43Zwpqx1Vb1mymnQR2YLPXzZR
+#
 
-if ! [ -x "$(command -v dotnet --list-sdks)" ]; then
-    cd /tmp
-    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-    sudo dpkg -i packages-microsoft-prod.deb
-    sudo add-apt-repository universe
-    sudo apt -y install apt-transport-https
-    sudo apt update
-    sudo apt -y install dotnet-sdk-2.2
-    cd
-fi
-if [ ! -d "X42-FullNode" ]; then
-    git clone https://github.com/x42protocol/X42-FullNode.git
-    cd X42-FullNode/src
-    dotnet restore
-    dotnet build --configuration Release
-    cd
-    mkdir ~/x42node
-    mv ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
-fi
-if [ ! -e "/etc/systemd/system/x42node.service" ]; then
-    sudo -i
-    cat <<EOF > /etc/systemd/system/x42node.service
+printf "\nChecking Linux version and if dotnet is installed...\n\n"
+if [ -x "$(command -v apt -v)" ]; then
+    osR="$(lsb_release -r -s)"
+    if [ ! -x "$(command -v dotnet --list-sdks)" ]; then
+        if [ $osR == "16.04" ]; then
+            printf "Using Ubuntu $osR.\n\ndotnet is not installed.\n\nInstalling now..\n\n"
+            cd /tmp
+            wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+            sudo dpkg -i packages-microsoft-prod.deb
+            sudo apt -y install apt-transport-https
+            sudo apt update
+            sudo apt -y install dotnet-sdk-2.2
+        elif [ $osR == "18.04" -o $osR == "18.10" ]; then
+            printf "Using Ubuntu $osR.\n\ndotnet is not installed.\n\nInstalling now..\n\n"
+            cd /tmp
+            wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+            sudo dpkg -i packages-microsoft-prod.deb
+            sudo add-apt-repository universe
+            sudo apt -y install apt-transport-https
+            sudo apt update
+            sudo apt -y install dotnet-sdk-2.2
+        elif [ $osR == "19.04" ]; then
+            printf "Using Ubuntu $osR.\n\ndotnet is not installed.\n\nInstalling now..\n\n"
+            cd /tmp
+            wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb
+            sudo dpkg -i packages-microsoft-prod.deb
+            sudo apt -y install apt-transport-https
+            sudo apt update
+            sudo apt -y install dotnet-sdk-2.2
+            if [ ! -x "$(command -v dotnet --list-sdks)" ]; then
+                printf "\nThere may have been an error while installing. Attempting alternative method...\n\n"
+                cd /tmp
+                sudo dpkg --purge packages-microsoft-prod && sudo dpkg -i packages-microsoft-prod.deb
+                sudo apt update
+                sudo apt -y install dotnet-sdk-2.2
+                if [ ! -x "$(command -v dotnet --list-sdks)" ]; then
+                    printf "\nThere may have been another error while installing dotnet.\n\nAttempting final alternative installation method provided by Microsoft...\n\n"
+                    cd /tmp
+                    sudo apt -y install gpg
+                    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+                    sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+                    wget -q https://packages.microsoft.com/config/ubuntu/19.04/prod.list
+                    sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+                    sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+                    sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+                    sudo apt -y install apt-transport-https
+                    sudo apt update
+                    sudo apt -y install dotnet-sdk-2.2
+                    if [ ! -x "$(command -v dotnet --list-sdks)" ]; then
+                        printf "\nIt appears dotnet is not installing properly.\n\nPlease review: https://dotnet.microsoft.com/download/linux-package-manager/ubuntu19-04/sdk-current\n\nPlease attempt installing dotnet manually using the information provided. Verify installation with 'dotnet --list-sdks' before running this script again.\n\nExiting.\n\n"
+                        exit 1
+                    fi
+                fi
+            fi
+        else
+            printf "You may not be on a correct Linux version.\n\nPlease try again later.\n\n"
+            exit 1
+        fi
+    else
+        printf "You're running a proper Ubuntu release ($osR) and dotnet is already installed.\n\n"
+    fi
+    if [ ! -d "/home/$USER/X42-FullNode" ]; then
+        printf "\nChecking if Git is installed...\n\n"
+        if [ ! -x "$(command -v git --version)" ]; then
+            printf "Git not installed, installing now...\n\n"
+            sudo apt -y install git
+        else
+            printf "Git already installed.\n\n"
+        fi
+        printf "Downloading X42-FullNode.\n\n"
+        git clone https://github.com/x42protocol/X42-FullNode.git ~/X42-FullNode
+        printf "\nBuilding FullNode...\n\n"
+        cd ~/X42-FullNode/src
+        dotnet restore
+        dotnet build --configuration Release
+        mkdir ~/x42node
+        mv ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
+    else
+        printf "X42-FullNode found, checking for updates before building...\n\n"
+        cd ~/X42-FullNode
+        git pull origin master
+        printf "\nBuilding FullNode...\n\n"
+        cd src
+        dotnet restore
+        dotnet build --configuration Release
+        if [ ! -d "/home/$USER/x42node" ]; then
+            mkdir ~/x42node
+            mv ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
+        else
+            mv -f ~/X42-FullNode/src/x42.x42D/bin/Release/netcoreapp2.1/*.* ~/x42node/
+        fi
+    fi
+    if [ ! -e "/etc/systemd/system/x42node.service" ]; then
+        printf "\nSetting up startup service.\n\n"
+        cat <<EOF > ~/x42node.service
 [Unit]
 Description=x42 Node
 [Service]
@@ -36,7 +117,18 @@ Environment=ASPNETCORE_ENVIRONMENT=Development
 [Install]
 WantedBy=multi-user.target
 EOF
-    systemctl start x42node.service
-    systemctl enable x42node.service
-    exit
+        sudo mv ~/x42node.service /etc/systemd/system/x42node.service
+        printf "\nStarting x42node.service\n\n"
+        sudo systemctl start x42node.service
+        printf "Verifying service started\n\n"
+        sleep 1.25
+        if [ "$(systemctl is-active x42node.service)" == "active" ]; then
+            printf "Started successfully. Enabling at startup...\n\n"
+            sudo systemctl enable x42node.service
+        else
+            printf "\nThere may be an error with starting the service.\n\nCheck the status using: sudo systemctl status x42node.service\n\n"
+            exit 1
+        fi
+        printf "\nThe x42-FullNode is now installed and running.\n\nTo stop use: sudo systemctl stop x42node.service\nCheck status use: sudo systemctl status x42node.service\nTo disable from startup: sudo systemctl disable x42node.service\n\nYou can check your Node by using: curl http://127.0.0.1:42220/api/Dashboard/Stats\n\nYou can access the Node API at: http://127.0.0.1:42220/swagger/index.html\n\n"
+    fi
 fi
